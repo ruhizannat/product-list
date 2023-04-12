4053ef43e502c9fba6e2844e72fa72b116680f8f
ruhi <ruhizannat71@gmail.com>
2023-04-13 02:27:17 +0600

    working with validation and reset the inputs

--- main.js
+++ main.js
@@ -17,12 +17,50 @@ const receiveInputsValue = () => {
 	};
 };
 
+const clearMessage = () => {
+	msgElm.textContent = '';
+};
+const showMessage = (msg, action) => {
+	const alertMsg = `<div class="alert alert-${action} mt-2" role="alert">
+	${msg}
+  </div>`;
+	msgElm.insertAdjacentHTML('afterbegin', alertMsg);
+	setTimeout(() => {
+		clearMessage();
+	}, 2000);
+};
+
+const validateInput = (name, price) => {
+	let isValid = true;
+
+	// check input is empty
+	if (name === '' || price === '') {
+		isValid = false;
+		showMessage('please provide necessary info', 'danger');
+	}
+
+	if (Number(price) !== Number(price)) {
+		isValid = false;
+		showMessage('please provide price in number', 'danger');
+	}
+	return isValid;
+};
+const resetInputs = () => {
+	nameInputElm.value = '';
+	priceInputElm.value = '';
+};
 
 formElm.addEventListener('submit', (e) => {
 	// browser reload prevent
 	e.preventDefault();
 
-	//receive inputs value
+	//receiving the inputs value
 	const { name, price } = receiveInputsValue();
-	console.log(name, price);
+
+	//check validation
+	const isValid = validateInput(name, price);
+	if (!isValid) return;
+
+	//reset the input
+	resetInputs();
 });
